433635573c58000ac91539094891bbe314e5b8fc
linohoch <linohoch@gmail.com>
2023-02-28 21:31:44 +0900

    cache with redis prac

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,40 @@
-import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  CACHE_MANAGER,
+  CacheInterceptor,
+  Controller,
+  Get,
+  Inject,
+  Post,
+  Request,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { Cache } from 'cache-manager';
+import { Public } from './public.decorator';
 
+// @UseInterceptors(CacheInterceptor)
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  testString = 'chached';
+  constructor(
+    private readonly appService: AppService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @Public()
+  @Get('cache')
+  async getCache(): Promise<string> {
+    const cachedData = await this.cacheManager.get('test');
+    if (cachedData) {
+      return String(cachedData) + 'from redis';
+    }
+    await this.cacheManager.set('test', this.testString, { ttl: 300 });
+    return this.testString;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,44 @@
-import { Module } from '@nestjs/common';
+import { CacheInterceptor, CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { BoardModule } from './board/board.module';
 import { AuthModule } from './auth/auth.module';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+// import * as redisStore from 'cache-manager-redis-store';
+import type { RedisClientOptions } from 'redis';
+import * as redisStore from 'cache-manager-ioredis';
 
 @Module({
-  imports: [UserModule, BoardModule, AuthModule],
+  imports: [
+    UserModule,
+    BoardModule,
+    AuthModule,
+    // CacheModule.register({
+    //   isGlobal: true,
+    //   ttl: 5,
+    //   max: 10,
+    // })
+    CacheModule.register<RedisClientOptions>({
+      store: redisStore,
+      host: 'localhost',
+      port: 6379,
+      isGlobal: true,
+      ttl: 5, // seconds
+      max: 10, // maximum number of items in cache
+    }),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
     },
   ],
 })
